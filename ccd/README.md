35c8af91f2201724c0420bf206030dfd64c9425d
Kevin Darlington <kdarlington@gmail.com>
2013-12-05 07:40:25 -0500

    Added some support for parsing Allergies.

--- ccd/README.md
+++ ccd/README.md
@@ -4,4 +4,4 @@ CCD
 Decodes Continuity of Care documents. Very preliminary right now. The API is constantly changing.
 
 
-This library will try not to fail. The medical industry a lot of times fails to follow specifications perfectly. This will try to parse what it can. It can't help serious XML errors though.
+This library will try not to fail. The medical industry a lot of times fails to follow specifications perfectly, so there is a lot of hackish code to parse what I can. It can't help serious XML errors though.
--- ccd/decode.go
+++ ccd/decode.go
@@ -10,6 +10,7 @@ import (
 
 // Template IDs
 var (
+	AllergiesTid     = []string{"2.16.840.1.113883.10.20.1.2", "2.16.840.1.113883.10.20.22.2.6.1"}
 	ImmunizationsTid = []string{"2.16.840.1.113883.10.20.1.6", "2.16.840.1.113883.10.20.22.2.2.1"}
 	MedicationsTid   = []string{"2.16.840.1.113883.10.20.1.8", "2.16.840.1.113883.10.20.22.2.1.1"}
 	ProblemsTid      = []string{"2.16.840.1.113883.10.20.1.11", "2.16.840.1.113883.10.20.22.2.5.1"}
@@ -22,6 +23,13 @@ var (
 		Type:     PARSE_DOC,
 		Priority: 0,
 		Func:     parsePatient,
+	}
+
+	AllergiesParser = Parser{
+		Type:     PARSE_SECTION,
+		Values:   AllergiesTid,
+		Priority: 0,
+		Func:     parseAllergies,
 	}
 
 	ImmunizationsParser = Parser{
@@ -60,7 +68,7 @@ var (
 	}
 
 	DefaultParsers = []Parser{
-		PatientParser, ImmunizationsParser,
+		PatientParser, AllergiesParser, ImmunizationsParser,
 		MedicationsParser, ProblemsParser,
 		ResultsParser, VitalSignsParser,
 	}
@@ -73,6 +81,7 @@ type CCD struct {
 	Problems      []Problem
 	Results       []Result
 	VitalSigns    []VitalSign
+	Allergies     []Allergy
 	Extra         interface{}
 
 	// Right now doc_parsers will only have one map entry "*"
@@ -200,7 +209,6 @@ func (c *CCD) ParseDoc(doc *xmlx.Document) error {
 			errs_ = p.Func(doc.Root, c)
 			//errs = append(errs, errs_...)
 		}
-
 	}
 
 	componentNode := Nget(doc.Root, "component", "structuredBody")
@@ -363,6 +371,86 @@ func parsePatient(root *xmlx.Node, ccd *CCD) []error {
 	}
 
 	return nil
+}
+
+type Allergy struct {
+	Name         string
+	Reaction     string
+	Status       string
+	SeverityCode string
+	SeverityText string
+}
+
+func parseAllergies(node *xmlx.Node, ccd *CCD) []error {
+	var errs []error
+
+	entryNodes := node.SelectNodes("*", "entry")
+	for _, entryNode := range entryNodes {
+		obvNode := Nget(entryNode, "act", "entryRelationship", "observation")
+		if obvNode == nil {
+			continue
+		}
+
+		allergy := Allergy{}
+
+		// Sometimes the substance is represented here
+		valueNode := Nget(obvNode, "value")
+		if valueNode != nil && valueNode.As("*", "codeSystem") == "2.16.840.1.113883.6.88" {
+			allergy.Name = valueNode.As("*", "displayName")
+		}
+
+		// Try to get substance another way
+		if len(allergy.Name) == 0 {
+			playNode := Nget(obvNode, "participant", "participantRole", "playingEntity")
+			if playNode != nil {
+				codeNode := Nget(playNode, "code")
+				if codeNode != nil {
+					allergy.Name = codeNode.As("*", "displayName")
+				}
+
+				if len(allergy.Name) == 0 {
+					nameNode := Nget(playNode, "name")
+					if nameNode != nil {
+						allergy.Name = nameNode.S("*", "name")
+					}
+				}
+			}
+		}
+
+		erNodes := obvNode.SelectNodes("*", "entryRelationship")
+		for _, erNode := range erNodes {
+			oNode := Nget(erNode, "observation")
+			codeNode := Nget(oNode, "code")
+			valueNode := Nget(oNode, "value")
+
+			switch {
+			// Reaction -- "Manifestation"
+			case erNode.As("*", "typeCode") == "MFST":
+				allergy.Reaction = valueNode.As("*", "displayName")
+
+				// Sometimes severity is a child of this observation
+				suboNode := Nget(oNode, "entryRelationship", "observation")
+				if suboNode != nil {
+					subValueNode := Nget(suboNode, "value")
+					allergy.SeverityCode = subValueNode.As("*", "code")
+					allergy.SeverityText = subValueNode.As("*", "displayName")
+				}
+
+			// Could be a Status or Severity -- Subject
+			case erNode.As("*", "typeCode") == "SUBJ":
+				if codeNode.As("*", "code") == "33999-4" { // Status
+					allergy.Status = valueNode.As("*", "displayName")
+				} else if codeNode.As("*", "code") == "SEV" { // Severity
+					allergy.SeverityCode = valueNode.As("*", "code")
+					allergy.SeverityText = valueNode.As("*", "displayName")
+				}
+			}
+		}
+
+		ccd.Allergies = append(ccd.Allergies, allergy)
+	}
+
+	return errs
 }
 
 type MedicationId struct {
