f9fe3f69387f69ddc433910ce7ba9093e152c230
James Munson <jmunson@gmail.com>
2016-01-28 16:16:00 -0600

    Reworked how problems works, added a test file.
    We now always parse the 'value' as Code, then override it with
    translation.
    
    This behavior seems wrong, but its giving me the best results out of all
    the ways I've tried.

--- ccd/problems.go
+++ ccd/problems.go
@@ -17,61 +17,83 @@ var (
 	}
 )
 
+//Problem represents an Observation Problem  (templateId: 2.16.840.1.113883.10.20.22.4.4)
 type Problem struct {
 	Name string
 	Date time.Time
 	// Duration time.Duration
-	Status string
-	Code   Code
+	Status      string
+	ProblemType string
+	Code        Code
 }
 
 func parseProblems(node *xmlx.Node, ccd *CCD) []error {
 	entryNodes := node.SelectNodes("*", "entry")
 	for _, entryNode := range entryNodes {
-		problem := Problem{}
-
 		observationNode := Nget(entryNode, "act", "entryRelationship", "observation")
-		valueNode := Nget(observationNode, "value")
-		if valueNode == nil {
-			continue
-		}
-		problem.Name = valueNode.As("*", "displayName")
-
-		translation := Nget(valueNode, "translation")
-		if translation != nil {
-			problem.Code.decode(translation)
-			//if the value node didnt have a displayName, try the translation node.
-			if problem.Name == "" && problem.Code.DisplayName != "" {
-				problem.Name = problem.Code.DisplayName
-			}
-		}
-
-		effectiveTimeNode := Nget(observationNode, "effectiveTime")
-		t := decodeTime(effectiveTimeNode)
-		problem.Date = t.Value
-
-		// observationNode2 := Nget(observationNode, "entryRelationship", "observation")
-		// if observationNode2 != nil {
-		//   problem.Status = Nget(observationNode2, "value").As("*", "displayName")
-		// }
-
-		erNodes := observationNode.SelectNodes("*", "entryRelationship")
-		for _, erNode := range erNodes {
-			oNode := Nget(erNode, "observation")
-			codeNode := Nget(oNode, "code")
-			valueNode := Nget(oNode, "value")
-
-			if codeNode == nil {
-				continue
-			}
-
-			if codeNode.As("*", "code") == "33999-4" { // Status
-				problem.Status = valueNode.As("*", "displayName")
-			}
-		}
+		problem := decodeProblem(observationNode)
 
 		ccd.Problems = append(ccd.Problems, problem)
 	}
 
 	return nil
+}
+
+func decodeProblem(node *xmlx.Node) Problem {
+	problem := Problem{}
+
+	valueNode := Nget(node, "value")
+	if valueNode == nil {
+		//the spec says there must be a value, but better to be safe than to panic.
+		return Problem{}
+	}
+	problem.Name = valueNode.As("*", "displayName")
+
+	//The spec seems to imply we should look at translation when value's nullFlavor is OTH
+	//in my testing, it appears we should always check for it.
+
+	//This current implementation decodes the value node to Code, then replaces it with its translation if it exists.
+	//This does not seem right, but seems to lead to the best results in our samples.
+	problem.Code.decode(valueNode)
+	if translation := Nget(valueNode, "translation"); translation != nil {
+		problem.Code.decode(translation)
+		if problem.Name == "" {
+			problem.Name = problem.Code.DisplayName
+		}
+
+	}
+
+	//get the problem type from the highest level code node
+	if topCode := Nget(node, "code"); topCode != nil {
+		name := topCode.As("*", "displayName")
+		if name == "" {
+			name = topCode.As("*", "code")
+		}
+		problem.ProblemType = name
+	}
+
+	effectiveTimeNode := Nget(node, "effectiveTime")
+	t := decodeTime(effectiveTimeNode)
+	problem.Date = t.Value
+
+	// observationNode2 := Nget(observationNode, "entryRelationship", "observation")
+	// if observationNode2 != nil {
+	//   problem.Status = Nget(observationNode2, "value").As("*", "displayName")
+	// }
+
+	erNodes := node.SelectNodes("*", "entryRelationship")
+	for _, erNode := range erNodes {
+		oNode := Nget(erNode, "observation")
+		codeNode := Nget(oNode, "code")
+		valueNode := Nget(oNode, "value")
+
+		if codeNode == nil {
+			continue
+		}
+
+		if codeNode.As("*", "code") == "33999-4" { // Status
+			problem.Status = valueNode.As("*", "displayName")
+		}
+	}
+	return problem
 }
---
+++ ccd/problems_test.go
@@ -0,0 +1,107 @@
+package ccd_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/kdar/health/ccd"
+	"github.com/kylelemons/godebug/pretty"
+)
+
+//parseTime returns a time.Time based on a premade layout
+func parseTime(in string) time.Time {
+	time, _ := time.Parse("2006-01-02", in)
+	return time
+
+}
+
+//TestParse_Problems parses a specific ccd with many different types of problems and compares it to a pre-made output.
+//This pre-made output may not be 100% correct, but this will let us know if something has changed.
+func TestParse_Problems(t *testing.T) {
+	c := ccd.NewDefaultCCD()
+	err := parseAndRecover(t, c, "testdata/specific/problems.xml", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	problems := []ccd.Problem{
+		ccd.Problem{
+			Name:        "Pneumonia",
+			Date:        parseTime("2012-08-06"),
+			Status:      "Active",
+			ProblemType: "Complaint",
+			Code: ccd.Code{
+				CodeSystemName: "",
+				Type:           "CD",
+				CodeSystem:     "2.16.840.1.113883.6.96",
+				Code:           "233604007",
+				DisplayName:    "Pneumonia",
+			},
+		},
+		ccd.Problem{
+			Name:        "CEREBRAL ARTERY OCCLUSION, UNSPECIFIED, WITH CEREBRAL INFARCTION",
+			Date:        parseTime("2009-07-09"),
+			Status:      "Active",
+			ProblemType: "Problem",
+			Code: ccd.Code{
+				CodeSystemName: "ICD-9-CM",
+				Type:           "",
+				CodeSystem:     "2.16.840.1.113883.6.104",
+				Code:           "434.91",
+				DisplayName:    "CEREBRAL ARTERY OCCLUSION, UNSPECIFIED, WITH CEREBRAL INFARCTION",
+			},
+		},
+		ccd.Problem{
+			Name:        "Body mass index 30+ - obesity",
+			Date:        parseTime("2011-01-18"),
+			Status:      "Active",
+			ProblemType: "",
+			Code: ccd.Code{
+				CodeSystemName: "SNOMED CT",
+				Type:           "",
+				CodeSystem:     "2.16.840.1.113883.6.96",
+				Code:           "162864005",
+				DisplayName:    "",
+			},
+		},
+		ccd.Problem{
+			Name:        "GERD (gastroesophageal reflux disease)",
+			Date:        parseTime("01-01-01"),
+			Status:      "Active",
+			ProblemType: "Problem",
+			Code: ccd.Code{
+				CodeSystemName: "ICD-9 CM",
+				Type:           "",
+				CodeSystem:     "2.16.840.1.113883.6.103",
+				Code:           "530.81",
+				DisplayName:    "GERD (gastroesophageal reflux disease)",
+			},
+		},
+		ccd.Problem{
+			Name:        "JOINT PAIN PELVIS",
+			Date:        parseTime("01-01-01"),
+			Status:      "Active",
+			ProblemType: "ASSERTION",
+			Code: ccd.Code{
+				Type:           "CD",
+				CodeSystem:     "2.16.840.1.113883.6.103",
+				CodeSystemName: "",
+				Code:           "719.45",
+				DisplayName:    "JOINT PAIN PELVIS",
+			},
+		}}
+
+	if len(problems) != len(c.Problems) {
+		t.Fatalf("Wrong number of problems in specific problems test. Expected: %d got: %d", len(problems), len(c.Problems))
+	}
+	for i, _ := range problems {
+		if !reflect.DeepEqual(problems[i], c.Problems[i]) {
+			fmt.Println(pretty.Compare(problems[i], c.Problems[i]))
+			t.Fatal()
+			//		t.Fatalf("Expected:\n%s, got:\n%s", goon.Sdump(problems), goon.Sdump(c.Problems))
+		}
+
+	}
+}
