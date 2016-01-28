2a77c679cd6de6d35960b80f82d38a7218b95a6e
James Munson <jmunson@gmail.com>
2016-01-28 16:10:37 -0600

    Improved encounters
    Location name is now parsed
    Initial Diagnosis support added (needs testing)

--- ccd/encounters.go
+++ ccd/encounters.go
@@ -7,25 +7,25 @@ import (
 )
 
 var (
-	EncountersTid = []string{"2.16.840.1.113883.10.20.22.2.22.1", "2.16.840.1.113883.10.20.22.4.49"}
-
+	TidEncounters    = []string{"2.16.840.1.113883.10.20.22.2.22.1", "2.16.840.1.113883.10.20.22.4.49"}
 	EncountersParser = Parser{
 		Type:     PARSE_SECTION,
-		Values:   EncountersTid,
+		Values:   TidEncounters,
 		Priority: 0,
 		Func:     parseEncounters,
 	}
-	SDLTid = "2.16.840.1.113883.10.20.22.4.32" //Service Delivery Location TemplateID
+	//The following TemplateID constants are used to find specific sections inside of an encounter.
+	TidSDL         = "2.16.840.1.113883.10.20.22.4.32" //TemplateID of the Service Delivery Location
+	TidEncounterDx = "2.16.840.1.113883.10.20.22.4.80" //TemplateID of the Encounter Diagnosis
 )
 
 type Encounter struct {
-	Name       string
 	Performers []Performer
 	Code       Code
 	Date       time.Time
 	Location   Location
-	Complaint  string
-	Diagnosis  string //todo: maybe support these
+	Diagnosis  Diagnosis
+	Complaint  string //todo: this, look for 'RSON'
 }
 
 type Location struct {
@@ -34,13 +34,18 @@ type Location struct {
 	Address Address
 	Telecom Telecom
 }
+type Diagnosis struct {
+	Code    Code
+	Status  string
+	Problem Problem
+	//TODO: add support for EffectiveTime
+}
 type Performer struct{}
 
 func parseEncounters(node *xmlx.Node, ccd *CCD) []error {
 	entryNodes := node.SelectNodes("*", "entry")
 	for _, entryNode := range entryNodes {
 		encounter := Encounter{}
-		encounter.Name = entryNode.Value
 		if code := Nget(entryNode, "code"); code != nil {
 			encounter.Code.decode(code)
 		}
@@ -53,8 +58,12 @@ func parseEncounters(node *xmlx.Node, ccd *CCD) []error {
 		//we loop through anything with a templateID inside of our main entry node
 		templates := entryNode.SelectNodes("*", "templateId")
 		for _, t := range templates {
-			if t.As("*", "root") == SDLTid {
+
+			switch t.As("*", "root") {
+			case TidSDL:
 				encounter.Location = decodeLocation(t.Parent)
+			case TidEncounterDx:
+				encounter.Diagnosis = decodeDiagnosis(t.Parent)
 			}
 		}
 
@@ -62,6 +71,20 @@ func parseEncounters(node *xmlx.Node, ccd *CCD) []error {
 	}
 
 	return nil
+}
+
+func decodeDiagnosis(node *xmlx.Node) Diagnosis {
+	d := Diagnosis{}
+	if code := Nget(node, "code"); code != nil {
+		d.Code.decode(code)
+	}
+	d.Status = Nget(node, "statusCode").As("*", "code")
+
+	if problem := Nget(node, "observation"); problem != nil {
+		d.Problem = decodeProblem(problem)
+	}
+
+	return d
 }
 
 func decodeLocation(node *xmlx.Node) Location {
@@ -75,6 +98,9 @@ func decodeLocation(node *xmlx.Node) Location {
 	}
 	if tNode := node.SelectNode("*", "telecom"); tNode != nil {
 		l.Telecom = decodeTelecom(tNode)
+	}
+	if peNode := node.SelectNode("*", "playingEntity"); peNode != nil {
+		l.Name = peNode.S("*", "name")
 	}
 
 	return l
